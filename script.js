5c1111e9359b9f3f0c2d9518673e1caee1761072
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-30 18:33:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let p = document.getElementById("status")
+let btn = document.getElementById("enterBtn")
+
+btn.addEventListener("click",() => {
+	p.textContent = "Entered Metaverse"
+})
