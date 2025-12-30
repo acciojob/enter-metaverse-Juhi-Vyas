4cf70e6036ddd0f428030401e30b5e7ebd4a6118
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-30 18:49:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let btn = document.getElementById("enterBtn")
 
 btn.addEventListener("click",() => {
 	let h1 = document.createElement("h1")
-	p.textContent =  "Entered Metaverse" 
-	h1.textContent =  p.textContent
+	p.textContent =  "" 
+	h1.textContent =  "Entered Metaverse"
+	p.appendChild(h1)
 	
 })
